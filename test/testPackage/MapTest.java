25c17995ddbb17cedf37fa4b08d3523df2709362
alexis <alexis.shan-yan@insa-lyon.fr>
2020-11-20 11:42:32 +0100

    Création du package testPackage

---
+++ test/testPackage/MapTest.java
@@ -0,0 +1,22 @@
+package testPackage;
+
+import junit.framework.TestCase;
+
+public class MapTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testGetIntersectionList() {
+    }
+
+    public void testGetSegmentList() {
+    }
+
+    public void testSetIntersectionList() {
+    }
+
+    public void testSetSegmentList() {
+    }
+}
