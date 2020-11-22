1d8800ab94a0746235d901857e9927571a5fd870
Emma <emmaneiss2@gmail.com>
2020-11-22 14:01:05 +0100

    Create ComputeTour.java

---
+++ src/processing/ComputeTour.java
@@ -0,0 +1,16 @@
+package processing;
+
+import objects.*;
+
+public class ComputeTour {
+
+    public static Tournee planTour() {
+
+        return null;
+    }
+
+    private static Tournee geneticATSP() {
+
+        return null;
+    }
+}
