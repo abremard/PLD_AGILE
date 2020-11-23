85a375df89bc220400e4e5d44833d4326e1199a9
thestefanristovski <stefan.ristovski1@gmail.com>
2020-11-23 17:52:02 +0100

    Added JavaFX UI To sample package:  - Added Main window with map displayed  - main view that loads xml files (working buttons and text field)  - timeline view  - ability to switch between views with button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,53 @@
 package sample;
 
+import com.sothawo.mapjfx.Projection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
 
+    /** Logger for the class */
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        logger.info("starting DemoApp");
+        String fxmlFile = "/sample/sample3_Fields.fxml";
+        logger.info("loading fxml file {}");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
+        logger.info("stage loaded");
+
+        final Controller controller = fxmlLoader.getController();
+        final Projection projection = getParameters().getUnnamed().contains("wgs84")
+                ? Projection.WGS_84 : Projection.WEB_MERCATOR;
+        controller.initMapAndControls(projection);
+
+        //controller.initCardContent();
+
+        Scene scene = new Scene(rootNode);
+        logger.info("scene created");
+
+
+
+        primaryStage.setTitle("Delivery UI Demo");
+        primaryStage.setScene(scene);
+        logger.info("showing scene");
         primaryStage.show();
+        logger.info("application start method finished.");
     }
 
 
     public static void main(String[] args) {
+        logger.info("begin main");
         launch(args);
+        logger.info("end main");
     }
 }
