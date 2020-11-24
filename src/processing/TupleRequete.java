cd337b5c11a468cd547e4c890ea5d308e40774b2
Emma <emmaneiss2@gmail.com>
2020-11-24 11:39:54 +0100

    Random path + useful points indexing
    :warning: not tested yet !

--- src/processing/TupleRequete.java
+++ src/processing/TupleRequete.java
@@ -2,6 +2,8 @@ package processing;
 
 import objects.Request;
 
+import java.util.Objects;
+
 public class TupleRequete {
 
     Request requete;
@@ -12,4 +14,17 @@ public class TupleRequete {
         this.isDepart = isDepart;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TupleRequete that = (TupleRequete) o;
+        return isDepart == that.isDepart &&
+                requete.equals(that.requete);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requete, isDepart);
+    }
 }
