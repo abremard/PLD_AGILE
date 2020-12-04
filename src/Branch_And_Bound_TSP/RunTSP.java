f14924c3ac61e176e1e8f2737b606b3eaab198d5
Yumahey <39220591+Yumahey@users.noreply.github.com>
2020-12-04 09:13:05 +0100

    B&B fonctionnel sans contraintes de précédence

--- src/Branch_And_Bound_TSP/RunTSP.java
+++ src/Branch_And_Bound_TSP/RunTSP.java
@@ -1,20 +1,20 @@
-
-package Branch_And_Bound_TSP;
-
-public class RunTSP {
-	public static void main(String[] args) {
-		TSP tsp = new TSP1();
-		for (int nbVertices = 8; nbVertices <= 16; nbVertices += 2){
-			System.out.println("Graphs with "+nbVertices+" vertices:");
-			Graph g = new CompleteGraph(nbVertices);
-			long startTime = System.currentTimeMillis();
-			tsp.searchSolution(20000, g);
-			System.out.print("Solution of cost "+tsp.getSolutionCost()+" found in "
-					+(System.currentTimeMillis() - startTime)+"ms : ");
-			for (int i=0; i<nbVertices; i++)
-				System.out.print(tsp.getSolution(i)+" ");
-			System.out.println("0");
-		}
-	}
-
-}
+//
+//package Branch_And_Bound_TSP;
+//
+//public class RunTSP {
+//	public static void main(String[] args) {
+//		TSP tsp = new TSP1();
+//		for (int nbVertices = 8; nbVertices <= 16; nbVertices += 2){
+//			System.out.println("Graphs with "+nbVertices+" vertices:");
+//			Graph g = new CompleteGraph(nbVertices);
+//			long startTime = System.currentTimeMillis();
+//			tsp.searchSolution(20000, g);
+//			System.out.print("Solution of cost "+tsp.getSolutionCost()+" found in "
+//					+(System.currentTimeMillis() - startTime)+"ms : ");
+//			for (int i=0; i<nbVertices; i++)
+//				System.out.print(tsp.getSolution(i)+" ");
+//			System.out.println("0");
+//		}
+//	}
+//
+//}
