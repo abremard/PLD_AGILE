fb5b9c74dabeae189897cecc305c49d856977209
Alex <bremard.alexandre@gmail.com>
2020-12-08 08:54:03 +0100

    fix tour state

--- src/state/TourState.java
+++ src/state/TourState.java
@@ -8,18 +8,19 @@ import objects.PlanningRequest;
 
 public class TourState implements State {
     public void newTour(ListOfCommands l, MVCController c) {
-        l.Add(new NewTourCommand());
+        l.Add(new NewTourCommand(), c);
         c.setCurrentState(c.getRequestState());
         if (debug) {
-            System.out.print(l.getI());
-            System.out.println(" - Adding New Tour Command from TourState to index ");
+            System.out.println(l.getI()+" - Adding New Tour Command from TourState to RequestState");
         }
     }
 
     public void modifyRequestList(MVCController c) {
         c.setCurrentState(c.getModifyState());
+        c.getL().setLowerBound(c.getL().getI()+1);
         if (debug) {
-            System.out.println("Going from TourState to ModifyState ");
+            System.out.println("Going from TourState to ModifyState");
+            System.out.println("Lower bound is set to "+(c.getL().getI()+1));
         }
     }
 
