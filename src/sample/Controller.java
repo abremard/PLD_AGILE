913d9671c76f782158beca3e5774ca33c426152d
Alex <bremard.alexandre@gmail.com>
2020-12-11 23:08:45 +0100

    junit add request command

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1298,6 +1298,34 @@ public class Controller {
         public String toString() {
             return name;
         }
+
+        @Override
+        public boolean equals(Object o) {
+
+            // If the object is compared with itself then return true
+            if (o == this) {
+                return true;
+            }
+
+        /* Check if o is an instance of Complex or not
+          "null instanceof [type]" also returns false */
+            if (!(o instanceof LocationTagContent)) {
+                return false;
+            }
+
+            // typecast o to Complex so that we can compare data members
+            LocationTagContent l = (LocationTagContent) o;
+
+            // Compare the data members and return accordingly
+            return name.equals(l.name)
+                    && streetOne.equals(l.streetOne)
+                    && streetTwo.equals(l.streetTwo)
+                    && arrivalTime.equals(l.arrivalTime)
+                    && coordLocation.equals(l.coordLocation)
+                    && request.equals(l.request)
+                    && chemin.equals(l.chemin)
+                    && Boolean.compare(isPickup, l.isPickup) == 0;
+        }
     }
 
     /**
